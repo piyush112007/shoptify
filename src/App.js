cc3863ba85be671618240bd9ccd8bc344e595d9e
Piyush Kumar Singh <2024ce114f@sigce.edu.in>
2025-07-13 00:12:33 +0530

    Changed idea of product.json due to vercel issue and add . js file to handle product source

--- src/App.js
+++ src/App.js
@@ -1,31 +1,23 @@
-import React, { useEffect, useState } from "react";
+import React, { useState } from "react";
 import "./App.css";
 import Tile from "./Components/tile";
-// import ProductData from "./Components/product";
+import ProductData from "./Components/product"; // Using imported product array
 import Navbar from "./Components/navbar";
 
 function App() {
   const [cart, setCart] = useState([]);
   const [showCart, setShowCart] = useState(false);
   const [currentBrand, setCurrentBrand] = useState("Reebok");
+
   const handleBrandSelect = (brand) => {
     setCurrentBrand(brand);
   };
-  const [products, setProducts] = useState([]);
 
-  useEffect(() => {
-    fetch("/data/products.json")
-      .then((response) => response.json())
-      .then((data) => {
-        const updatedData = data.map((product) => ({
-          ...product,
-          image: process.env.PUBLIC_URL + product.image,
-        }));
-        setProducts(updatedData);
-      })
-      .catch((error) => console.error("Error loading products:", error));
-  }, []);
-  const Products = products.filter((product) => product.Brand === currentBrand);
+  // Filter products based on current brand
+  const Products = ProductData.filter(
+    (product) => product.Brand === currentBrand
+  );
+
   const handleAdd = (product) => setCart((prev) => [...prev, product]);
   const handleRemove = (product) =>
     setCart((prev) => prev.filter((p) => p !== product));
--- src/Components/product.js
+++ src/Components/product.js
@@ -1,12 +1,380 @@
-import dunkLow from "./Images/Nike/Dunk-Low.jpg";
-import AirJordanOB from "./Images/Nike/Air-Jordan-Low Orange Black.jpg";
-import AirJordanHIRT from "./Images/Nike/Air Jordan 1 high Retro.jpg";
-import AirJordanMOM from "./Images/Nike/Air jordan 1 Low MOM.jpg";
+// Nike Imports
+import AirJordanLow from "./Images/Nike/Air-Jordan-Low Orange Black.jpg";
+import AirJordanHighRetro from "./Images/Nike/Air Jordan 1 high Retro.jpg";
+import AirJordanLowMOM from "./Images/Nike/Air jordan 1 Low MOM.jpg";
 import AirJordanTOD from "./Images/Nike/Air Jordan TOD.jpg";
-import AirFORCELORT from "./Images/Nike/Air force low retro.jpg";
+import AirForceLowRetro from "./Images/Nike/Air force low retro.jpg";
 import AirForceLV8 from "./Images/Nike/Nike air force LV8.jpg";
-import AirFORCEDANCE from "./Images/Nike/Air force Da.jpg";
+import AirForceDance from "./Images/Nike/Air force Da.jpg";
+import NikeDunkLow from "./Images/Nike/Dunk-Low.jpg";
+import AirMaxDn8 from "./Images/Nike/air-max-dn8-shoes-3gEhDeZq.jpg";
 
-const Products = [];
+// Adidas Imports
+import AirRollStroll from "./Images/Addidas/Air-RollStroll_Black_JK3811_01_00_standard.jpg";
+import CopaPure3 from "./Images/Addidas/Copa_Pure_3_Club_Turf_Boots_Black_JR2893_22_model.jpg";
+import CushAppM from "./Images/Addidas/Cush-App_M_White_JK2271_01_00_standard.jpg";
+import LiteRacerAdaptKids from "./Images/Addidas/Lite_Racer_Adapt_7.0_Shoes_Kids_Purple_JR0818_01_00_standard.jpg";
+import SambaOGBlack from "./Images/Addidas/Samba_OG_Shoes_Black_JR0910_01_00_standard.jpg";
+import SambaOGWhite from "./Images/Addidas/Samba_OG_Shoes_White_IG1024_01_standard.jpg";
+import FuseoMen from "./Images/Addidas/Fuseo Men(Running).jpg";
+import Solez from "./Images/Addidas/solez.jpg";
+import AdizeroEVOSL from "./Images/Addidas/Adizero_EVO_SL_Shoes_White_JH6206_01_00_standard.jpg";
 
+// Puma Imports
+import CourtClassicLux from "./Images/Puma/Court-Classic-Lux-Sneakers.jpg";
+import DarterProFade from "./Images/Puma/Darter-Pro-Fade-Running-Shoes.jpg";
+import HyperglideX from "./Images/Puma/Hyperglide-X-FusionLite-Everyday-Running-Shoes.jpg";
+import SkyrocketLite from "./Images/Puma/Skyrocket-Lite-Men's-Running-Shoes.jpg";
+import SwiftPulseTraining1 from "./Images/Puma/Swift-Pulse-Men's-Training-Shoes.jpg";
+import SwiftPulseTraining2 from "./Images/Puma/Swift-Pulse-Men's-Training-Shoes(2).jpg";
+import XRay2Square from "./Images/Puma/X-Ray-2-Square-Superior-Comfort-Sneakers.jpg";
+import PorscheLegacyRSX from "./Images/Puma/Porsche-Legacy-RS-X-Sneakers.jpg";
+import ScuderiaFerrariFutureCat from "./Images/Puma/Scuderia-Ferrari-Future-Cat-Mid-Motorsport-Sneakers.jpg";
+
+// Reebok Imports
+import NanoX5Unisex from "./Images/Reebok/100209367_SLC_eCom-tif.png";
+import NanoX5Female from "./Images/Reebok/100209375_SLC_eCom-tif.png";
+import AnswerIII from "./Images/Reebok/100209505_SLC_eCom-tif.png";
+import NanoX5Female2 from "./Images/Reebok/100212599_SLC_eCom-tif.png";
+import EngineABasketball1 from "./Images/Reebok/100218464_SLC_eCom-tif.png";
+import ReebokAnuelAA from "./Images/Reebok/100239410_SLC_eCom-tif.png";
+import EngineABasketball2 from "./Images/Reebok/JPG-100218465_SLC_eCom.jpg";
+import ReebokAngelReese from "./Images/Reebok/JPG-100228937_SLC_eCom.jpg";
+import NanoX5Female3 from "./Images/Reebok/JPG-100238429_SLC_eCom.jpg";
+
+const Products = [
+  {
+    title: "Air Jordan 1 Low",
+    subtitle: "Men's Shoes",
+    image: AirJordanLow,
+    Brand: "Nike",
+    Description:
+      "A pastel-toned Nike Dunk Low with rose pink overlays and navy accents for a clean, stylish look.",
+    price: "₹8 995.00",
+  },
+  {
+    title: "Air Jordan 1 Retro High OG",
+    subtitle: "Men's Shoes",
+    image: AirJordanHighRetro,
+    Brand: "Nike",
+    Description:
+      "A pastel-toned Nike Dunk Low with rose pink overlays and navy accents for a clean, stylish look.",
+    price: "₹16 995.00",
+  },
+  {
+    title: "Air Jordan 1 Low Method of Make",
+    subtitle: "Men's Shoes",
+    image: AirJordanLowMOM,
+    Brand: "Nike",
+    Description:
+      "A pastel-toned Nike Dunk Low with rose pink overlays and navy accents for a clean, stylish look.",
+    price: "₹8 995.00",
+  },
+  {
+    title: "Air Jordan 1 Low ALT",
+    subtitle: "Baby Shoes",
+    image: AirJordanTOD,
+    Brand: "Nike",
+    Description:
+      "A pastel-toned Nike Dunk Low with rose pink overlays and navy accents for a clean, stylish look.",
+    price: "₹4 395.00",
+  },
+  {
+    title: "Air Force 1 Low Retro",
+    subtitle: "Men's Shoes",
+    image: AirForceLowRetro,
+    Brand: "Nike",
+    Description:
+      "A pastel-toned Nike Dunk Low with rose pink overlays and navy accents for a clean, stylish look.",
+    price: "₹13 995.00",
+  },
+  {
+    title: "Air Force 1 '07 LV8",
+    subtitle: "Men's Shoes",
+    image: AirForceLV8,
+    Brand: "Nike",
+    Description:
+      "A pastel-toned Nike Dunk Low with rose pink overlays and navy accents for a clean, stylish look.",
+    price: "₹10 795.00",
+  },
+  {
+    title: "Air Force 1 Dance",
+    subtitle: "Men's Shoes",
+    image: AirForceDance,
+    Brand: "Nike",
+    Description:
+      "A pastel-toned Nike Dunk Low with rose pink overlays and navy accents for a clean, stylish look.",
+    price: "₹11 895.00",
+  },
+  {
+    title: "Nike Dunk Low SE",
+    subtitle: "Women's Shoes",
+    image: NikeDunkLow,
+    Brand: "Nike",
+    Description:
+      "A pastel-toned Nike Dunk Low with rose pink overlays and navy accents for a clean, stylish look.",
+    price: "₹11 895.00",
+  },
+  {
+    title: "Nike Air Max Dn8",
+    subtitle: "Men's Shoes",
+    image: AirMaxDn8,
+    Brand: "Nike",
+    Description:
+      "A pastel-toned Nike Dunk Low with rose pink overlays and navy accents for a clean, stylish look.",
+    price: "₹17 495.00",
+  },
+  {
+    title: "Air-Roll Stroll",
+    subtitle: "Men's Running",
+    image: AirRollStroll,
+    Brand: "Adidas",
+    Description:
+      "A pastel-toned Nike Dunk Low with rose pink overlays and navy accents for a clean, stylish look.",
+    price: "₹3 599.00",
+  },
+  {
+    title: "Air-Roll Stroll",
+    subtitle: "Men's Running",
+    image: AirRollStroll,
+    Brand: "Adidas",
+    Description:
+      "A pastel-toned Nike Dunk Low with rose pink overlays and navy accents for a clean, stylish look.",
+    price: "₹3 599.00",
+  },
+  {
+    title: "Copa Pure 3 Club Turf Boots",
+    subtitle: "Football",
+    image: CopaPure3,
+    Brand: "Adidas",
+    Description:
+      "A pastel-toned Nike Dunk Low with rose pink overlays and navy accents for a clean, stylish look.",
+    price: "₹5 599.00",
+  },
+  {
+    title: "Cush-App M",
+    subtitle: "Men's Running",
+    image: CushAppM,
+    Brand: "Adidas",
+    Description:
+      "A pastel-toned Nike Dunk Low with rose pink overlays and navy accents for a clean, stylish look.",
+    price: "₹2 309.00",
+  },
+  {
+    title: "Lite Racer Adapt 7.0 Shoes Kids",
+    subtitle: "Kids Lifestyle",
+    image: LiteRacerAdaptKids,
+    Brand: "Adidas",
+    Description:
+      "A pastel-toned Nike Dunk Low with rose pink overlays and navy accents for a clean, stylish look.",
+    price: "₹4 999.00",
+  },
+  {
+    title: "Samba OG Shoes",
+    subtitle: "Orignals",
+    image: SambaOGBlack,
+    Brand: "Adidas",
+    Description:
+      "A pastel-toned Nike Dunk Low with rose pink overlays and navy accents for a clean, stylish look.",
+    price: "₹10 999.00",
+  },
+  {
+    title: "Samba OG Shoes",
+    subtitle: "Orignals",
+    image: SambaOGWhite,
+    Brand: "Adidas",
+    Description:
+      "A pastel-toned Nike Dunk Low with rose pink overlays and navy accents for a clean, stylish look.",
+    price: "₹10 999.00",
+  },
+  {
+    title: "Fuseo Men",
+    subtitle: "Men's Running",
+    image: FuseoMen,
+    Brand: "Adidas",
+    Description:
+      "A pastel-toned Nike Dunk Low with rose pink overlays and navy accents for a clean, stylish look.",
+    price: "₹3 299.00",
+  },
+  {
+    title: "SOLEZ",
+    subtitle: "Lifestyle",
+    image: Solez,
+    Brand: "Adidas",
+    Description:
+      "A pastel-toned Nike Dunk Low with rose pink overlays and navy accents for a clean, stylish look.",
+    price: "₹2 299.00",
+  },
+  {
+    title: "Adizero EVO SL Shoes",
+    subtitle: "Men's Running",
+    image: AdizeroEVOSL,
+    Brand: "Adidas",
+    Description:
+      "A pastel-toned Nike Dunk Low with rose pink overlays and navy accents for a clean, stylish look.",
+    price: "₹16 999.00",
+  },
+  {
+    title: "Court Classic Lux Sneakers",
+    subtitle: "Men's Sneakers",
+    image: CourtClassicLux,
+    Brand: "Puma",
+    Description:
+      "A pastel-toned Nike Dunk Low with rose pink overlays and navy accents for a clean, stylish look.",
+    price: "₹3 299.00",
+  },
+  {
+    title: "Darter Pro Fade Running Shoes",
+    subtitle: "Men's Running",
+    image: DarterProFade,
+    Brand: "Puma",
+    Description:
+      "A pastel-toned Nike Dunk Low with rose pink overlays and navy accents for a clean, stylish look.",
+    price: "₹4 199.00",
+  },
+  {
+    title: "Hyperglide X FusionLite Everyday Running Shoes",
+    subtitle: "Men's Running",
+    image: HyperglideX,
+    Brand: "Puma",
+    Description:
+      "A pastel-toned Nike Dunk Low with rose pink overlays and navy accents for a clean, stylish look.",
+    price: "₹3 009.00",
+  },
+  {
+    title: "Skyrocket Lite Men's Running Shoes",
+    subtitle: "Men's Running",
+    image: SkyrocketLite,
+    Brand: "Puma",
+    Description:
+      "A pastel-toned Nike Dunk Low with rose pink overlays and navy accents for a clean, stylish look.",
+    price: "₹2 499.00",
+  },
+  {
+    title: "Swift Pulse Men's Training Shoes",
+    subtitle: "Men's Training",
+    image: SwiftPulseTraining1,
+    Brand: "Puma",
+    Description:
+      "A pastel-toned Nike Dunk Low with rose pink overlays and navy accents for a clean, stylish look.",
+    price: "₹3 499.00",
+  },
+  {
+    title: "Swift Pulse Men's Training Shoes",
+    subtitle: "Men's Training",
+    image: SwiftPulseTraining2,
+    Brand: "Puma",
+    Description:
+      "A pastel-toned Nike Dunk Low with rose pink overlays and navy accents for a clean, stylish look.",
+    price: "₹3 499.00",
+  },
+  {
+    title: "X-Ray 2 Square Comfort Sneakers",
+    subtitle: "Men's Sneakers",
+    image: XRay2Square,
+    Brand: "Puma",
+    Description:
+      "A pastel-toned Nike Dunk Low with rose pink overlays and navy accents for a clean, stylish look.",
+    price: "₹3 599.00",
+  },
+  {
+    title: "Porsche Legacy RS-X Sneakers",
+    subtitle: "Men's Motorsports",
+    image: PorscheLegacyRSX,
+    Brand: "Puma",
+    Description:
+      "A pastel-toned Nike Dunk Low with rose pink overlays and navy accents for a clean, stylish look.",
+    price: "₹5 399.00",
+  },
+  {
+    title: "Scuderia Ferrari Future Cat Mid",
+    subtitle: "Men's Motorsports",
+    image: ScuderiaFerrariFutureCat,
+    Brand: "Puma",
+    Description:
+      "A pastel-toned Nike Dunk Low with rose pink overlays and navy accents for a clean, stylish look.",
+    price: "₹16 999.00",
+  },
+  {
+    title: "Nano X5 Training Shoes",
+    subtitle: "Unisex Sports",
+    image: NanoX5Unisex,
+    Brand: "Reebok",
+    Description:
+      "A pastel-toned Nike Dunk Low with rose pink overlays and navy accents for a clean, stylish look.",
+    price: "₹15 999.00",
+  },
+  {
+    title: "Nano X5 Training Shoes",
+    subtitle: "Female Sports",
+    image: NanoX5Female,
+    Brand: "Reebok",
+    Description:
+      "A pastel-toned Nike Dunk Low with rose pink overlays and navy accents for a clean, stylish look.",
+    price: "₹15 999.00",
+  },
+  {
+    title: "Answer III Shoes",
+    subtitle: "Unisex Classics",
+    image: AnswerIII,
+    Brand: "Reebok",
+    Description:
+      "A pastel-toned Nike Dunk Low with rose pink overlays and navy accents for a clean, stylish look.",
+    price: "$140.00",
+  },
+  {
+    title: "Nano X5 Training Shoes",
+    subtitle: "Female Sports",
+    image: NanoX5Female2,
+    Brand: "Reebok",
+    Description:
+      "A pastel-toned Nike Dunk Low with rose pink overlays and navy accents for a clean, stylish look.",
+    price: "₹15 999.00",
+  },
+  {
+    title: "Engine A Basketball Shoes",
+    subtitle: "Unisex Sports",
+    image: EngineABasketball1,
+    Brand: "Reebok",
+    Description:
+      "A pastel-toned Nike Dunk Low with rose pink overlays and navy accents for a clean, stylish look.",
+    price: "₹17 999.00",
+  },
+  {
+    title: "Reebok x Anuel AA BB 4000 II",
+    subtitle: "Unisex Classics",
+    image: ReebokAnuelAA,
+    Brand: "Reebok",
+    Description:
+      "A pastel-toned Nike Dunk Low with rose pink overlays and navy accents for a clean, stylish look.",
+    price: "₹8 599.00",
+  },
+  {
+    title: "Engine A Basketball Shoes",
+    subtitle: "Unisex Sports",
+    image: EngineABasketball2,
+    Brand: "Reebok",
+    Description:
+      "A pastel-toned Nike Dunk Low with rose pink overlays and navy accents for a clean, stylish look.",
+    price: "₹17 999.00",
+  },
+  {
+    title: "Reebok x Angel Reese Shaqnosis",
+    subtitle: "Unisex Sports",
+    image: ReebokAngelReese,
+    Brand: "Reebok",
+    Description:
+      "A pastel-toned Nike Dunk Low with rose pink overlays and navy accents for a clean, stylish look.",
+    price: "₹9 999.00",
+  },
+  {
+    title: "Nano X5 Training Shoes",
+    subtitle: "Female Shoes",
+    image: NanoX5Female3,
+    Brand: "Reebok",
+    Description:
+      "A pastel-toned Nike Dunk Low with rose pink overlays and navy accents for a clean, stylish look.",
+    price: "₹15 999.00",
+  },
+];
 export default Products;
